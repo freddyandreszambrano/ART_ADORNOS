a65e16e6161ba2822f1ba6c104661e83314e20cb
Freddy Zambrano <freddyfazq0614@gmail.com>
2024-09-12 01:01:21 -0500

    style: :sparkles: agregar interfaz de login
    creacion de interfaz de login solo frontend

--- frontend/src/pages/Account.js
+++ frontend/src/pages/Account.js
@@ -2,13 +2,80 @@ import Navbar from "../components/Navbar";
 
 export default function AccountPage() {
     return (
-
         <div className="bg-zinc-100 dark:bg-gray-900 flex-auto text-gray-900 dark:text-white flex flex-col">
             <Navbar />
             <section className="text-center my-16 mx-8 flex-auto">
-            <h1 className="text-5xl font-extrabold mb-16">Cuenta</h1>          
+                <h1 className="text-5xl font-extrabold ">Bienvenidos Adornos ART</h1>
 
+                <div className="flex min-h-full flex-col justify-center px-6  lg:px-8">
+                    <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                        <form className="space-y-6" action="#" method="POST">
+                            <div>
+                                <label
+                                    htmlFor="email"
+                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                >
+                                    Your email
+                                </label>
+                                <div className="mt-2">
+                                    <input
+                                        type="email"
+                                        id="email"
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                                        placeholder="name@flowbite.com"
+                                    />
+                                </div>
+                            </div>
+
+                            <div>
+                                <div className="flex items-center justify-between">
+                                    <label
+                                        htmlFor="password"
+                                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                    >
+                                        Your password
+                                    </label>
+                                    <div className="text-sm">
+                                        <a
+                                            href="#"
+                                            className="font-semibold text-indigo-600 hover:text-indigo-500"
+                                        >
+                                            Forgot password?
+                                        </a>
+                                    </div>
+                                </div>
+                                <div className="mt-2">
+                                    <input
+                                        type="password"
+                                        id="password"
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                                        placeholder="••••••••"
+                                    />
+                                </div>
+                            </div>
+
+                            <div>
+                                <button
+                                    type="submit"
+                                    className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                >
+                                    Sign in
+                                </button>
+                            </div>
+                        </form>
+
+                        <p className="mt-10 text-center text-sm text-gray-500">
+                            Not a member?{" "}
+                            <a
+                                href="#"
+                                className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+                            >
+                                Start a 14 day free trial
+                            </a>
+                        </p>
+                    </div>
+                </div>
             </section>
         </div>
     );
-} 
+}
